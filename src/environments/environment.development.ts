07d247d182374b1f1b97741e58dfddcab4726844
leosrehacek <leos.rehacek@epptec.eu>
2023-12-01 17:29:42 +0100

    update for deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  apiUrl: "http://localhost:8080",
   availableLanguages: ['en', 'cs'],
   languageDebug: true,
   defaultLanguage: 'cs'
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  apiUrl: "https://api.vr-dashboard.leosrehacek.com",
   availableLanguages: ['en', 'cs'],
   languageDebug: false,
   defaultLanguage: 'cs'
